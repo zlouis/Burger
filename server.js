458a5d375eae2bb67ff18e4252be27442ffc8118
Louis Huang <jiannchyao@gmail.com>
2016-11-04 11:38:47 -0400

    Initial server.js

---
+++ server.js
@@ -0,0 +1,5 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+// var mysql = require('mysql');
+// var handlebars = require('express-handlebars');
