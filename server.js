6dcd56c805d6c9309e103083d330ef527b4e0a02
Louis Huang <jiannchyao@gmail.com>
2016-11-08 22:12:27 -0500

    Working node server.js

--- server.js
+++ server.js
@@ -1,5 +1,36 @@
+var Sequelize = require('sequelize'), connection;
+if (process.env.JAWSDB_URL) {
+  connection = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  connection = new Sequelize('burgers_db', 'root', 'password', {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: '3000'
+  })
+}
+
 var express = require('express');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
-// var mysql = require('mysql');
-// var handlebars = require('express-handlebars');
+
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static(process.cwd() + '/public'));
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+// override with POST having ?_method=DELETE
+app.use(methodOverride('_method'));
+var exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+var routes = require('./controllers/burgers_controller.js');
+app.use('/', routes);
+
+var port = 3000;
+app.listen(port);
