41890dd239f2f0305fa0a86adefdc53d08a4e699
Louis Huang <jiannchyao@gmail.com>
2016-11-08 21:16:16 -0500

    Create seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,24 @@
+
+INSERT INTO burgers (burger_name) VALUES ('Bachi Burger');
+INSERT INTO burgers (burger_name) VALUES ('Brindle Burger');
+INSERT INTO burgers (burger_name) VALUES ('Americana Burger');
+INSERT INTO burgers (burger_name) VALUES ('The Company Burger');
+
+INSERT INTO menu (item) VALUES ('Bachi Burger');
+INSERT INTO menu (item) VALUES ('Brindle Burger');
+INSERT INTO menu (item) VALUES ('Americana Burger');
+INSERT INTO menu (item) VALUES ('The Company Burger');
+INSERT INTO menu (item) VALUES ('The Counter Burger');
+INSERT INTO menu (item) VALUES ('The Emmy Burger');
+INSERT INTO menu (item) VALUES ('Classic Burger');
+INSERT INTO menu (item) VALUES ('Husk Burger');
+INSERT INTO menu (item) VALUES ('Velveeta Burger');
+INSERT INTO menu (item) VALUES ('Kua'Aina Burger');
+
+INSERT INTO menu (item, entree) VALUES ('Waffle Fries', 0);
+INSERT INTO menu (item, entree) VALUES ('Onion Rings', 0);
+INSERT INTO menu (item, entree) VALUES ('Onion Rings', 0);
+INSERT INTO menu (item, entree) VALUES ('Chocolate Mint Milk Shake', 0);
+INSERT INTO menu (item, entree) VALUES ('Vanilla Milk Shake', 0);
+INSERT INTO menu (item, entree) VALUES ('Blackberry Milk Shake', 0);
+INSERT INTO menu (item, entree) VALUES ('Cookes and Cream Milk Shake', 0);
